c79f73d5f2fc73158e540ea019a3ff592b2183f5
Neamar <neamar@neamar.fr>
2012-04-24 21:36:18 +0200

    Generic views for docs

--- docs/urls.py
+++ docs/urls.py
@@ -1,6 +1,13 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import DetailView, ListView
+from docs.models import Rule
 
 urlpatterns = patterns('',
-    url(r'^$', 'docs.views.index', name='home'),
-    url(r'^(?P<rule_name>[a-z-]+)$', 'docs.views.detail'),
+	url(r'^$',
+		ListView.as_view(
+			queryset=Rule.objects.all())),
+	url(r'^(?P<slug>[a-z-]+)$',
+		DetailView.as_view(
+			model=Rule),
+		name='docs_rule'),
 )
--- docs/views.py
+++ docs/views.py
@@ -1,11 +1,2 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from docs.models import Rule
-
-def index(request):
-	rule_list = Rule.objects.all()
-
-	return render_to_response('docs/index.html', {'rule_list': rule_list})
-
-def detail(request, rule_name):
-	rule = get_object_or_404(Rule, slug=rule_name)
-	return render_to_response('docs/detail.html', {'rule': rule})

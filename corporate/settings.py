1a9296be868f4f9dd8adcf24771283e7f7d2ac8a
Neamar <neamar@neamar.fr>
2012-04-25 17:43:54 +0200

    Ajout des fixtures

--- corporate/settings.py
+++ corporate/settings.py
@@ -120,10 +120,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.markup',
-    # Uncomment the next line to enable the admin:
     'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    # Modules
     'corporate',
     'docs',
 )

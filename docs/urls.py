6155621cb611676dedde5c9e2acd10a3c86ea826
Neamar <neamar@neamar.fr>
2012-05-30 16:31:35 +0200

    Effects

--- docs/urls.py
+++ docs/urls.py
@@ -1,11 +1,16 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import DetailView, ListView
 from docs.models import Rule
+from corpos.models import Corporation
 
 urlpatterns = patterns('',
 	url(r'^$',
 		ListView.as_view(
 			queryset=Rule.objects.all())),
+	url(r'^effets$',
+		ListView.as_view(
+			queryset=Corporation.objects.all(),
+			 template_name='docs/effets.html')),
 	url(r'^(?P<slug>[a-z-]+)$',
 		DetailView.as_view(
 			model=Rule),
